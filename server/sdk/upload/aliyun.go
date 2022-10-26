7079f4ec1a04bcf028457b0a582752de1c93903e
hb <hb1707@live.cn>
2022-10-26 22:14:10 +0800

    新增oss copy

--- server/sdk/upload/aliyun.go
+++ server/sdk/upload/aliyun.go
@@ -31,7 +31,36 @@ func (*AliyunOSS) Copy(ori string, new string) error {
 	if err != nil {
 		return errors.New("AliyunOSS.Copy().NewBucket() Error:" + err.Error())
 	}
-	_, err = bucket.CopyObject(new, ori)
+	options := []oss.Option{
+		oss.MetadataDirective(oss.MetaReplace),
+		//oss.Expires(expires),
+		//oss.SetTagging(taggingInfo),
+		// 指定复制源Object的对象标签到目标 Object。
+		// oss.TaggingDirective(oss.TaggingCopy),
+		// 指定创建目标Object时的访问权限ACL为私有。
+		// oss.ObjectACL(oss.ACLPrivate),
+		// 指定KMS托管的用户主密钥，该参数仅在x-oss-server-side-encryption为KMS时有效。
+		//oss.ServerSideEncryptionKeyID("9468da86-3509-4f8d-a61e-6eab1eac****"),
+		// 指定OSS创建目标Object时使用的服务器端加密算法。
+		// oss.ServerSideEncryption("AES256"),
+		// 指定复制源Object的元数据到目标Object。
+		//oss.MetadataDirective(oss.MetaCopy),
+		// 指定CopyObject操作时是否覆盖同名目标Object。此处设置为true，表示禁止覆盖同名Object。
+		// oss.ForbidOverWrite(true),
+		// 如果源Object的ETag值和您提供的ETag相等，则执行拷贝操作，并返回200 OK。
+		//oss.CopySourceIfMatch("5B3C1A2E053D763E1B002CC607C5****"),
+		// 如果源Object的ETag值和您提供的ETag不相等，则执行拷贝操作，并返回200 OK。
+		//oss.CopySourceIfNoneMatch("5B3C1A2E053D763E1B002CC607C5****"),
+		// 如果指定的时间早于文件实际修改时间，则正常拷贝文件，并返回200 OK。
+		//oss.CopySourceIfModifiedSince(2021-12-09T07:01:56.000Z),
+		// 如果指定的时间等于或者晚于文件实际修改时间，则正常拷贝文件，并返回200 OK。
+		//oss.CopySourceIfUnmodifiedSince(2021-12-09T07:01:56.000Z),
+		// 指定Object的存储类型。此处设置为Standard，表示标准存储类型。
+		//oss.StorageClass("Standard"),
+	}
+
+	// 使用指定的元信息覆盖源文件的元信息。
+	_, err = bucket.CopyObject(new, ori, options...)
 	if err != nil {
 		return errors.New("AliyunOSS.Copy().CopyObject() Error:" + err.Error())
 	}
