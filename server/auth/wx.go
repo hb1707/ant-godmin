faec004539dcd790b619f120a2eb24efa99d466d
hb <hb1707@live.cn>
2024-04-23 18:51:49 +0800

    perf:微信api调用缓存优化

--- server/auth/wx.go
+++ server/auth/wx.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/hb1707/ant-godmin/pkg/log"
 	"github.com/hb1707/ant-godmin/setting"
 	"github.com/silenceper/wechat/v2"
@@ -9,13 +10,21 @@ import (
 	miniConfig "github.com/silenceper/wechat/v2/miniprogram/config"
 	offConfig "github.com/silenceper/wechat/v2/officialaccount/config"
 	offJS "github.com/silenceper/wechat/v2/officialaccount/js"
+	"github.com/silenceper/wechat/v2/util"
 	workConfig "github.com/silenceper/wechat/v2/work/config"
 	workJS "github.com/silenceper/wechat/v2/work/js"
 	"github.com/silenceper/wechat/v2/work/oauth"
 	"github.com/silenceper/wechat/v2/work/user"
+	"time"
 )
 
+var cacheGetMpAccessTokenTime = make(map[string]time.Time)
+var cacheMpAccessToken = make(map[string]string)
+
 func GetMpAccessToken(appId string) (string, error) {
+	if time.Since(cacheGetMpAccessTokenTime[appId]) < time.Hour && cacheMpAccessToken[appId] != "" {
+		return cacheMpAccessToken[appId], nil
+	}
 	wc := wechat.NewWechat()
 	memory := cache.NewMemory()
 	cfg := &offConfig.Config{
@@ -25,10 +34,23 @@ func GetMpAccessToken(appId string) (string, error) {
 	}
 	official := wc.GetOfficialAccount(cfg)
 	res, err := official.GetAccessToken()
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+	cacheGetMpAccessTokenTime[appId] = time.Now()
+	cacheMpAccessToken[appId] = res
 	return res, err
 }
 
+var cacheGetMpJsConfigTime = make(map[string]time.Time)
+var cacheMpJsConfig = make(map[string]*offJS.Config)
+
 func GetMpJsConfig(appId, url string) (*offJS.Config, error) {
+	var cacheKey = appId + url
+	if time.Since(cacheGetMpJsConfigTime[cacheKey]) < time.Hour && cacheMpJsConfig[cacheKey] != nil {
+		return cacheMpJsConfig[cacheKey], nil
+	}
 	wc := wechat.NewWechat()
 	memory := cache.NewMemory()
 	cfg := &offConfig.Config{
@@ -37,8 +59,37 @@ func GetMpJsConfig(appId, url string) (*offJS.Config, error) {
 		Cache:     memory,
 	}
 	official := wc.GetOfficialAccount(cfg)
+	accessToken, err := GetMpAccessToken(appId)
+	if err != nil {
+		return nil, err
+	}
+
 	js := official.GetJs()
+
+	/* todo 有问题，跳过了缓存
 	config, err := js.GetConfig(url)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}*/
+
+	var ticketStr string
+	ticketStr, err = js.GetTicket(accessToken)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	nonceStr := util.RandomStr(16)
+	timestamp := util.GetCurrTS()
+	str := fmt.Sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%d&url=%s", ticketStr, nonceStr, timestamp, url)
+	sigStr := util.Signature(str)
+	var config = new(offJS.Config)
+	config.AppID = js.AppID
+	config.NonceStr = nonceStr
+	config.Timestamp = timestamp
+	config.Signature = sigStr
+	cacheGetMpJsConfigTime[cacheKey] = time.Now()
+	cacheMpJsConfig[cacheKey] = config
 	return config, err
 }
 
@@ -100,7 +151,15 @@ func GetQyWxConfig(appid, url string) (conf *workJS.Config) {
 	}
 	return
 }
+
+var cacheGetQyWxAgentConfigTime = make(map[string]time.Time)
+var cacheQyWxAgentConfig = make(map[string]*workJS.Config)
+
 func GetQyWxAgentConfig(appid, url string) (conf *workJS.Config) {
+	var cacheKey = appid + url
+	if time.Since(cacheGetQyWxAgentConfigTime[cacheKey]) < time.Hour && cacheQyWxAgentConfig[cacheKey] != nil {
+		return cacheQyWxAgentConfig[cacheKey]
+	}
 	wc := wechat.NewWechat()
 	memory := cache.NewMemory()
 	cfg := &workConfig.Config{
@@ -116,6 +175,8 @@ func GetQyWxAgentConfig(appid, url string) (conf *workJS.Config) {
 		log.Error(err)
 		return
 	}
+	cacheGetQyWxAgentConfigTime[cacheKey] = time.Now()
+	cacheQyWxAgentConfig[cacheKey] = conf
 	return
 }
 func GetQyUser(appid, userID string) (*user.Info, error) {
