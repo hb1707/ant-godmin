36c9fc386eba3eefdf19644fecd19d691468b99f
hb <hb1707@live.cn>
2024-11-21 19:10:57 +0800

    fix:修复编译bug

--- setting/app.go
+++ setting/app.go
@@ -2,7 +2,7 @@ package setting
 
 import (
 	"encoding/json"
-	"github.com/hb1707/ant-godmin/pkg/log"
+	"log"
 	"os"
 )
 
@@ -16,14 +16,14 @@ func ReadAppInfo(path string) AppInfo {
 	// Read app.json
 	b, err := os.ReadFile(path)
 	if err != nil {
-		log.Error(err)
+		log.Println("[ERROR]", err)
 		return AppInfo{}
 	}
 	// Unmarshal app.json
 	var appInfo AppInfo
 	err = json.Unmarshal(b, &appInfo)
 	if err != nil {
-		log.Error(err)
+		log.Println("[ERROR]", err)
 		return AppInfo{}
 	}
 	return appInfo
