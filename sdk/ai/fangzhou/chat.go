0933c3d029bb96ef516d402ab058ba52c0b4f757
hb <hb1707@live.cn>
2025-01-10 19:17:55 +0800

    feat:开启stream流

--- sdk/ai/fangzhou/chat.go
+++ sdk/ai/fangzhou/chat.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hb1707/ant-godmin/pkg/log"
 	"github.com/volcengine/volcengine-go-sdk/service/arkruntime"
 	"github.com/volcengine/volcengine-go-sdk/service/arkruntime/model"
+	"io"
 )
 
 type Config struct {
@@ -13,11 +14,13 @@ type Config struct {
 
 type Client struct {
 	Config
+	StreamChan chan model.ChatCompletionStreamResponse
 }
 
 func NewChat(config Config) *Client {
 	return &Client{
-		Config: config,
+		Config:     config,
+		StreamChan: make(chan model.ChatCompletionStreamResponse),
 	}
 }
 
@@ -39,4 +42,35 @@ func (c *Client) Chat(endpointId string, messages []*model.ChatCompletionMessage
 		return nil, err
 	}
 	return resp.Choices, nil
+}
+
+func (c *Client) ChatStream(endpointId string, messages []*model.ChatCompletionMessage) error {
+	client := arkruntime.NewClientWithApiKey(
+		c.ApiKey,
+	)
+	// 创建一个上下文，通常用于传递请求的上下文信息，如超时、取消等
+	ctx := context.Background()
+	// 构建聊天完成请求，设置请求的模型和消息内容
+	req := model.ChatCompletionRequest{
+		Model:    endpointId,
+		Messages: messages,
+		Stream:   true,
+	}
+	stream, err := client.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		log.Error("standard chat error: %v", err)
+		return err
+	}
+	defer stream.Close()
+	for {
+		recv, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Error("standard chat error: %v", err)
+			return err
+		}
+		c.StreamChan <- recv
+	}
 }
