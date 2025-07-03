2bf28df787e8866896d01adc9889c360a2a43337
wind <hb@onecat.cn>
2025-07-03 12:12:38 +0800

    feat: 增加文档智能大模型处理状态标记

--- model/file.go
+++ model/file.go
@@ -6,30 +6,40 @@ import (
 	"strings"
 )
 
+type FileStatus string
+
+const (
+	FileStatusDefault FileStatus = ""
+	FileStatusDoing   FileStatus = "D"
+	FileStatusSuccess FileStatus = "S"
+	FileStatusFailed  FileStatus = "F"
+)
+
 type Files struct {
-	TypeId    uint             `json:"type_id" gorm:"type:tinyint UNSIGNED not null;default:0;comment:分类id"`        //分类id 0 图片 2其他 3证件 4APK
-	CloudType consts.CloudType `json:"cloud_type" gorm:"type:tinyint UNSIGNED not null;default:0;comment:云类型"`      //云类型
-	FileType  consts.FileType  `json:"file_type" gorm:"type:tinyint UNSIGNED not null;default:0;comment:文件类型"`      //文件类型,0 未知，1 图片，2 JSON
-	Domain    string           `json:"domain" gorm:"type:varchar(64) not null;default:'';comment:域名"`               //域名
-	UserSpace string           `json:"user_space" gorm:"type:varchar(64) not null;default:'';comment:用户空间"`         //用户空间
-	Uid       uint             `json:"uid" gorm:"type:int UNSIGNED not null;default:0;comment:用户id"`                //用户id
-	From      string           `json:"from" gorm:"type:varchar(255) not null;default:'';comment:用户来源"`              //用户来源
-	Name      string           `json:"name" gorm:"type:varchar(255) not null;default:'';comment:文件名"`               // 文件名
-	Url       string           `json:"url" gorm:"type:varchar(255) not null;default:'';comment:文件地址"`               // 文件地址
-	Tag       string           `json:"tag" gorm:"type:varchar(255) not null;default:'';comment:文件标签"`               // 文件标签
-	Key       string           `json:"key" gorm:"type:varchar(255) not null;default:'';comment:编号"`                 // 编号
+	TypeId    uint             `json:"type_id" gorm:"type:tinyint UNSIGNED not null;default:0;comment:分类id"`              //分类id 0 图片 2其他 3证件 4APK
+	CloudType consts.CloudType `json:"cloud_type" gorm:"type:tinyint UNSIGNED not null;default:0;comment:云类型"`           //云类型
+	FileType  consts.FileType  `json:"file_type" gorm:"type:tinyint UNSIGNED not null;default:0;comment:文件类型"`          //文件类型,0 未知，1 图片，2 JSON
+	Domain    string           `json:"domain" gorm:"type:varchar(64) not null;default:'';comment:域名"`                     //域名
+	UserSpace string           `json:"user_space" gorm:"type:varchar(64) not null;default:'';comment:用户空间"`             //用户空间
+	Uid       uint             `json:"uid" gorm:"type:int UNSIGNED not null;default:0;comment:用户id"`                      //用户id
+	From      string           `json:"from" gorm:"type:varchar(255) not null;default:'';comment:用户来源"`                  //用户来源
+	Name      string           `json:"name" gorm:"type:varchar(255) not null;default:'';comment:文件名"`                    // 文件名
+	Url       string           `json:"url" gorm:"type:varchar(255) not null;default:'';comment:文件地址"`                   // 文件地址
+	Tag       string           `json:"tag" gorm:"type:varchar(255) not null;default:'';comment:文件标签"`                   // 文件标签
+	Key       string           `json:"key" gorm:"type:varchar(255) not null;default:'';comment:编号"`                       // 编号
 	TempExist bool             `json:"temp_exist" gorm:"type:tinyint UNSIGNED not null;default:0;comment:临时文件是否存在"` // 临时文件是否存在
-	Other     FileOther        `json:"other" gorm:"type:json;comment:其他信息"`                                         //其他信息
-	Content   string           `json:"content" gorm:"type:longtext;comment:文件内容"`                                   //文件内容
-	Cover     string           `json:"cover" gorm:"type:varchar(255) not null;default:'';comment:封面"`               //封面
+	Other     FileOther        `json:"other" gorm:"type:json;comment:其他信息"`                                             //其他信息
+	Content   string           `json:"content" gorm:"type:longtext;comment:文件内容"`                                       //文件内容
+	Cover     string           `json:"cover" gorm:"type:varchar(255) not null;default:'';comment:封面"`                     //封面
+	Status    FileStatus       `json:"state" gorm:"type:tinyint UNSIGNED not null;default:0;comment:文件状态"`              //文件状态
 	Author    string           `json:"author" gorm:"-"`
 	FileId    uint             `json:"file_id" gorm:"-"`
 	TableBase
 }
 type FilesTemp struct {
 	FileId uint   `json:"file_id" gorm:"type:int UNSIGNED not null;default:0;comment:源文件id"` //源文件id
-	Url    string `json:"url" gorm:"type:varchar(255) not null;default:'';comment:文件地址"`     //文件地址
-	Key    string `json:"key" gorm:"type:varchar(255) not null;default:'';comment:编号"`       // 编号
+	Url    string `json:"url" gorm:"type:varchar(255) not null;default:'';comment:文件地址"`    //文件地址
+	Key    string `json:"key" gorm:"type:varchar(255) not null;default:'';comment:编号"`        // 编号
 	TableBase
 }
 
