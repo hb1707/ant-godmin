a99cd548dc41fc5c8f777936cd0a647db1f490e4
wind <hb@onecat.cn>
2025-07-10 18:50:55 +0800

    feat: 增加appinfo.json修改逻辑

--- setting/app.go
+++ setting/app.go
@@ -7,12 +7,13 @@ import (
 )
 
 type AppInfo struct {
-	Name    string
-	Version string
-	Desc    string
-	Channel string
+	Name    string `json:"name"`
+	Version string `json:"version"`
+	Desc    string `json:"desc"`
+	Channel string `json:"channel"`
 }
 
+// ReadAppInfo 读取app.json
 func ReadAppInfo(path string) AppInfo {
 	// Read app.json
 	b, err := os.ReadFile(path)
@@ -28,4 +29,31 @@ func ReadAppInfo(path string) AppInfo {
 		return AppInfo{}
 	}
 	return appInfo
+}
+
+// WriteAppInfo 写入app.json
+func WriteAppInfo(path string, appInfo AppInfo) {
+	var exist = ReadAppInfo(path)
+	if appInfo.Name != "" {
+		exist.Name = appInfo.Name
+	}
+	if appInfo.Version != "" {
+		exist.Version = appInfo.Version
+	}
+	if appInfo.Desc != "" {
+		exist.Desc = appInfo.Desc
+	}
+	if appInfo.Channel != "" {
+		exist.Channel = appInfo.Channel
+	}
+	b, err := json.MarshalIndent(exist, "", "  ")
+	if err != nil {
+		log.Println("[ERROR]", err)
+		return
+	}
+	err = os.WriteFile(path, b, 0644)
+	if err != nil {
+		log.Println("[ERROR]", err)
+		return
+	}
 }
