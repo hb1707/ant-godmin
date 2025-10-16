a727850919a60ac8a043ccf867df0f113cf2d32f
wind <hb@onecat.cn>
2025-10-16 11:13:25 +0800

    perf: 多服务就绪间隔10秒

--- routers/json/app.go
+++ routers/json/app.go
@@ -1,6 +1,8 @@
 package json
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hb1707/ant-godmin/common"
 )
@@ -12,7 +14,16 @@ func AppTest(c *gin.Context) {
 	return
 }
 
+var timeStart time.Time
+
 func AppBegin(c *gin.Context) {
+	if !timeStart.IsZero() && time.Since(timeStart) < time.Second*10 {
+		c.JSON(500, gin.H{
+			"message": "not ready",
+		})
+		return
+	}
+	timeStart = time.Now()
 	common.ReadinessNotice()
 	c.JSON(200, gin.H{
 		"message": "pong",
