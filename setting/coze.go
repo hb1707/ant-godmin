16495995208cd31beb012606c9c60bc8a8f25991
hb <hb@onecat.cn>
2025-12-10 03:21:28 +0800

    feat: 增加coze团队版配置

--- setting/coze.go
+++ setting/coze.go
@@ -4,11 +4,20 @@ var Coze struct {
 	ClientId     string
 	ClientSecret string
 }
+var CozeTeam struct {
+	ClientId     string
+	ClientSecret string
+}
 
 func confCoze() {
 	cz, err := Cfg.GetSection("coze")
 	if err == nil {
 		Coze.ClientId = cz.Key("CLIENT_ID").String()
 		Coze.ClientSecret = cz.Key("CLIENT_SECRET").String()
+	}
+	czTeam, err := Cfg.GetSection("coze_team")
+	if err == nil {
+		CozeTeam.ClientId = czTeam.Key("CLIENT_ID").String()
+		CozeTeam.ClientSecret = czTeam.Key("CLIENT_SECRET").String()
 	}
 }
