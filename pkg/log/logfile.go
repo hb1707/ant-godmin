a86799fccc769c0e1026651dc8cb8c52f8bcef4b
hb <hb@onecat.cn>
2026-07-23 13:59:08 +0800

    refactor(logfile): 移除调试模式检查，简化调试函数

--- pkg/log/logfile.go
+++ pkg/log/logfile.go
@@ -123,9 +123,6 @@ func Info(er ...interface{}) {
 }
 
 func Debug(er ...interface{}) {
-	if setting.App.RUNMODE != "dev" && setting.App.RUNMODE != "debug" {
-		return
-	}
 	log.SetPrefix("[ DEBUG ]")
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	file, err := os.OpenFile(LPath+"debug_"+time.Now().Format("20060102")+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
